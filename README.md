3c02d8e603df732c1fdf39c7f115821c8b5b0eba
fhag <gerard.fischer@outlook.com>
2020-01-11 12:34:35 +0100

    Updated .gitignore and README.md

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# Get to the documentation [doc] (https://github.com/fhag/solar/build/html/index)
-
-Inline-style:
-![alt text](https://github.com/fhag/solar/tree/master/build/html/_images/inenvelope.png "Logo Title Text 1")
-
-![text] (https://github.com/fhag/solar/blob/master/build/html/_images/inenvelope.png "bild text")
+test
--- solar/charge.py
+++ solar/charge.py
@@ -52,7 +52,7 @@ from solar.car import Car
 from solar.definitions.access_data import EMAIL, PW, VIN, HOME
 from solar.send_status import send_status
 
-__version__ = '0.1.57'
+__version__ = '0.1.58'
 print(f'charge v{__version__}')
 
 logger = logging.getLogger(__name__)
@@ -220,7 +220,6 @@ class ChargeEV(ChargeDefaults):
             except AssertionError:
                 logger.warning(
                     'Internet connection established after %.0f sec', i)
-                time.sleep(sleeptime)
                 return True
             except Exception:
                 logger.debug('Unexpected Exception after %.0f seconds', i)
