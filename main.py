ab227ec96df02fef9ac8a572647912c4c351b599
fhag <gerard.fischer@outlook.com>
2022-05-15 07:46:12 +0200

    main termination indicated

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ solar power available
 @author: gfi
 """
 import logging
+import time
 from solar.charge import ChargeEV
 from solar.definitions.logger_config import logger
 
 
 NAME = 'solar'
 logger.setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 print(f'{__name__:40s} v{__version__}')
 
 if __name__ == '__main__':
     ev = ChargeEV()
     ev.run()
+    logger.warning(f'Main program terminated at {time.asctime()}')
+    logger.warning('=' * 100)
